a93d2661f4e9bece6e9e8e4c4c4e23c15c621098
Rithik Jain <rithik.jain3006@gmail.com>
2020-03-29 17:46:26 +0530

    user service done

--- pkg/user/service.go
+++ pkg/user/service.go
@@ -1,1 +1,43 @@
 package user
+
+type Service interface {
+	Register(user *User) (*User, error)
+
+	GetUserByID(id float64) (*User, error)
+
+	GetRepo() Repository
+}
+
+type service struct {
+	repo Repository
+}
+
+func NewService(r Repository) Service {
+	return &service{
+		repo: r,
+	}
+}
+
+func (s *service) Register(user *User) (*User, error) {
+	exists, err := s.repo.DoesEmailExist(user.Email)
+	if err != nil {
+		return nil, err
+	}
+	if exists {
+		//noinspection GoErrorStringFormat
+		u, err := s.repo.FindByEmail(user.Email)
+		if err != nil {
+			return nil, err
+		}
+		return u, nil
+	}
+	return s.repo.Register(user)
+}
+
+func (s *service) GetUserByID(id float64) (*User, error) {
+	return s.repo.FindByID(id)
+}
+
+func (s *service) GetRepo() Repository {
+	return s.repo
+}
