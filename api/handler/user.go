35803526b4db3d9bed6298a865ae9db898f59fbc
Rithik Jain <rithik.jain3006@gmail.com>
2020-03-30 13:01:59 +0530

    Added view user details route

--- api/handler/user.go
+++ api/handler/user.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"encoding/json"
 	"github.com/dgrijalva/jwt-go"
+	"github.com/rithikjain/GistsBackend/api/middleware"
 	"github.com/rithikjain/GistsBackend/api/view"
 	"github.com/rithikjain/GistsBackend/pkg/user"
 	"net/http"
@@ -50,6 +51,36 @@ func register(s user.Service) http.Handler {
 	})
 }
 
+// Protected request
+func userDetails(s user.Service) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			view.Wrap(view.ErrMethodNotAllowed, w)
+			return
+		}
+
+		claims, err := middleware.ValidateAndGetClaims(r.Context(), "user")
+		if err != nil {
+			view.Wrap(err, w)
+			return
+		}
+		u, err := s.GetUserByID(claims["id"].(float64))
+		if err != nil {
+			view.Wrap(err, w)
+			return
+		}
+
+		w.Header().Add("Content-Type", "application/json; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		_ = json.NewEncoder(w).Encode(map[string]interface{}{
+			"message": "User Found",
+			"status":  http.StatusOK,
+			"user":    u,
+		})
+	})
+}
+
 func MakeUserHandler(r *http.ServeMux, svc user.Service) {
 	r.Handle("/api/user/register", register(svc))
+	r.Handle("/api/user/details", middleware.Validate(userDetails(svc)))
 }
